37b67b7893d9d971e8cc731f61511d0388c102fd
reschoene <reschoene@gmail.com>
2021-01-13 19:51:18 -0300

    first commit

---
+++ MarioBros/desktop/src/br/com/reschoene/mariobros/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.com.reschoene.mariobros.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.com.reschoene.mariobros.MarioBros;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MarioBros(), config);
+	}
+}
