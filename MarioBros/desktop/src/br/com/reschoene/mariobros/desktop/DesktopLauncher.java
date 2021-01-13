54fbc67e8d79444c001adbb55f8103c8dd544b94
reschoene <reschoene@gmail.com>
2021-01-13 20:19:29 -0300

    change gdx-backend-lwjgl to gdx-backend-lwjgl3 to solve an issue

--- MarioBros/desktop/src/br/com/reschoene/mariobros/desktop/DesktopLauncher.java
+++ MarioBros/desktop/src/br/com/reschoene/mariobros/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
 package br.com.reschoene.mariobros.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import br.com.reschoene.mariobros.MarioBros;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MarioBros(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new MarioBros(), config);
 	}
 }
